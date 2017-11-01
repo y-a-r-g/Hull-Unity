bb86340864c4195a909775cb2bd929ee7515a19c
Sergey Dvoynikov <svdvoynikov@gmail.com>
2017-11-01 16:22:51 +0500

    MainThread helper added

---
+++ MainThread.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using UnityEngine;
+
+namespace Plugins {
+    public class MainThread : MonoBehaviour {
+        private readonly Queue<Action> _queue = new Queue<Action>();
+
+        private static readonly object Lock = new object();
+        private static MainThread _instance;
+
+        private static MainThread Instance {
+            get {
+                lock (Lock) {
+                    if (!_instance) {
+                        _instance = FindObjectOfType<MainThread>();
+                    }
+                    if (!_instance) {
+                        _instance = new GameObject("MainThread").AddComponent<MainThread>();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public static void Init() {
+            var instance = Instance;
+        }
+
+        public static void Invoke(Action action) {
+            Instance._queue.Enqueue(action);
+        }
+
+        private void Update() {
+            while (_queue.Count > 0) {
+                var action = _queue.Dequeue();
+                action();
+            }
+        }
+    }
+}
